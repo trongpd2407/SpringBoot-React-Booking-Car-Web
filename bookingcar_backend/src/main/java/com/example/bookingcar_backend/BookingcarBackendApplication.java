33c074fd09e12b7593c2b8204a403e99af3f3da2
trongpd2407 <116246648+trongpd2407@users.noreply.github.com>
2023-05-16 15:39:50 +0700

    commit

---
+++ bookingcar_backend/src/main/java/com/example/bookingcar_backend/BookingcarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookingcar_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingcarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingcarBackendApplication.class, args);
+    }
+
+}
---
+++ bookingcar_backend/src/test/java/com/example/bookingcar_backend/BookingcarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookingcar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingcarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
